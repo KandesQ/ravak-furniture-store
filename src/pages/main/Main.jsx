8a76ad6b057387e65cb88188f6ca6355645227fb
Fedor Kapranov <fedor.kapranov.06@gmail.com>
2026-08-18 20:43:43 +0300

    Hero

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,11 +1,13 @@
 
 import Header from "./header/Header.jsx"
+import Hero from "./hero/Hero.jsx"
 
 export default function Main()
 {
     return (
     <>
        <Header />
+       <Hero />
     </> 
     )
 }
---
+++ src/pages/main/hero/Hero.jsx
@@ -0,0 +1,24 @@
+import "./Hero.css"
+
+export default function Hero()
+{
+    return (
+        <div className="hero">
+            <div className="hero__background"></div>
+
+            <div className="hero__container">
+                <div className="hero__content">
+                    <div className="hero__wrapper">
+                        <h1 className="hero__title">Дизайнерская сантехника Ravak</h1>
+                        <p className="hero__description">Чешское качество, лаконичные формы и гарантия до 10 лет. Собрали ванную мечты — от чаши до последнего крепления.</p>
+                    </div>
+                    
+                    <button className="hero__button">Cмотреть коллекцию</button>
+                    
+                </div>
+                
+                <div className="right__container"></div>
+            </div>
+        </div>
+    )
+}
